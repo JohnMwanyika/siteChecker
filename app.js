6590282a5150e7c98fdaa4d9bf198d752e9a002e
John Mwanyika <mwanyikajohn@outlook.com>
2023-06-16 09:11:35 +0300

    Setup session middleware

--- app.js
+++ app.js
@@ -3,14 +3,27 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// define the connection string
+const { sequelize } = require('./config/config');
 // getting routers from reouter folder
 const indexRouter = require('./routes/index.route');
 const dashboardRouter = require('./routes/dashboard.route');
 const usersRouter = require('./routes/users');
 
 const app = express();
+// configure session middleware
+app.use(session({
+  secret: ' THIS$is@my%littleSecret.Iwanna/SHareiTWithYouGuys,DoYouWann@He@rIt.com',
+  resave: false,
+  saveUninitialized: false,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+}))
 
+// sequelize.sync().then(data=>console.log(data)).catch(err=>console.log(err));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -24,7 +37,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/dashboard',dashboardRouter);
+app.use('/dashboard', dashboardRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
