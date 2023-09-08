0df2080b6ef0e1fa746ad5253c8f5891c9df7b57
John Mwanyika <mwanyikajohn@outlook.com>
2023-09-08 14:51:51 +0300

    add overlay spinner when loading new monitors

--- public/assets/spinner/spinner.js
+++ public/assets/spinner/spinner.js
@@ -2,12 +2,22 @@
 
 // Function to show the overlay spinner
 function showOverlaySpinner() {
-    const overlaySpinner = document.querySelector('.overlay-spinner');
-    overlaySpinner.style.display = 'flex';
-  }
-  
-  // Function to hide the overlay spinner
-  function hideOverlaySpinner() {
-    const overlaySpinner = document.querySelector('.overlay-spinner');
-    overlaySpinner.style.display = 'none';
-  }
+  const overlaySpinner = document.querySelector('.overlay-spinner');
+  overlaySpinner.style.display = 'flex';
+};
+
+// Function to hide the overlay spinner
+function hideOverlaySpinner() {
+  const overlaySpinner = document.querySelector('.overlay-spinner');
+  overlaySpinner.style.display = 'none';
+};
+
+function showSpinner(element) {
+  const overlaySpinner = document.querySelector(element);
+  overlaySpinner.style.display = 'flex';
+};
+
+function hideSpinner(element) {
+  const overlaySpinner = document.querySelector(element);
+  overlaySpinner.style.display = 'none';
+};
