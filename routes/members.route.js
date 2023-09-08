dca5cbbefb791e5c37e48d6f168dca0899f67334
John Mwanyika <mwanyikajohn@outlook.com>
2023-09-08 12:37:31 +0300

    protect member routes from csrf

--- routes/members.route.js
+++ routes/members.route.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { allMembers, getMembersApi, createMemberApi, updateMemberApi, deleteMemberApi } = require('../controllers/dashboard.controller');
+const { generateCsrfToken, validateCsrfToken } = require('../middlewares/auth.mid');
 const router = express.Router();
 
-router.get('/', allMembers);
+router.use(validateCsrfToken);
+router.get('/', generateCsrfToken, allMembers);
 router.get('/api', getMembersApi);
 router.post('/api/', createMemberApi);
 router.post('/api/update/:memberId', updateMemberApi);
